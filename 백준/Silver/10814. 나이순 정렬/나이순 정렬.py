a24fda477a0b64762833941f503376e4e6892e49
hee26 <132039586+hee26@users.noreply.github.com>
2023-11-07 16:01:51 +0900

    [Silver V] Title: 나이순 정렬, Time: 8040 ms, Memory: 48188 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,18 +1,14 @@
 import sys
 
 N = int(sys.stdin.readline())
-agelst = []
 lst = []
 
 for _ in range(N):
     age, name = map(str, input().split())
-    agelst.append(int(age))
+    age = int(age)
     lst.append([age, name])
 
+lst.sort(key=lambda x: x[0])
 
-agelst = sorted(set(agelst))
-
-for i in agelst:
-    for j in range(N):
-        if i == int(lst[j][0]):
-            print(*lst[j])
+for a, b in lst:
+    print(a, b)
