28a4073b244a303b4403b3010e6aecca668976d4
hee26 <132039586+hee26@users.noreply.github.com>
2024-01-03 22:07:13 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 380 ms, Memory: 110620 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
@@ -0,0 +1,27 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+A = list(map(int, sys.stdin.readline().split()))
+sorted = False
+cnt = 0
+
+while not sorted:
+    sorted = True
+
+    for i in range(N-1):
+        if A[i] > A[i+1]:
+            A[i], A[i+1] = A[i+1], A[i]
+            sorted = False
+            cnt += 1
+
+        if cnt == K:
+            print(A[i], A[i+1])
+            break
+
+    if cnt == K:
+        break
+
+    N -= 1
+
+if cnt < K:
+    print(-1)
