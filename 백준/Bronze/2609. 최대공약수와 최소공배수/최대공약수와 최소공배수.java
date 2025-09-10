979ec5e2f0df93450e044970d2737c40651e50c9
HeeJae <132039586+1026hz@users.noreply.github.com>
2025-09-10 10:05:28 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,34 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-
+public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
 
-        int ans1 = gcd(a,b);
-
-        bw.write(ans1 + "\n");
-        bw.write(a*b/ans1 + "");
-        bw.flush();
-
-        bw.close();
-        br.close();
-
-    }
-
-    public static int gcd(int a, int b){
-        int tmp;
-        while (b > 0){
+        int tmp = 0;
+        int a = n1;
+        int b = n2;
+        while(b > 0){
             tmp = a % b;
             a = b;
             b = tmp;
         }
-        return a;
+        int gcd = a;
+        int lcm = n1*n2/gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+
+
+
     }
 }
