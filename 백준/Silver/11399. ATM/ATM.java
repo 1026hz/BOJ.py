86346520d12e3afa7d17e2bdf72e15bbe3edb31c
HeeJae <132039586+1026hz@users.noreply.github.com>
2025-11-03 22:20:28 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,24 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] people = new int[n];
-        int now = 0;
-        int ans = 0;
+        int N = Integer.parseInt(br.readLine());
+        int[] people = new int[N+1];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i=0; i<n; i++){
-            int tmp = Integer.parseInt(st.nextToken());
-            people[i] = tmp;
+        for(int i=1; i<=N; i++){
+            people[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(people);
 
-        for(int i=0; i<n; i++){
-            now += people[i];
-            ans += now;
+        Arrays.sort(people);
+        int sum = 0;
+        int ans = 0;
+        for(int i=1; i<=N; i++){
+            sum += people[i];
+            ans += sum;
         }
 
         System.out.println(ans);
